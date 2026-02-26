f7fafb82cb83c32675dab440d0ca47da845ccc0e
sakshiibhise-thinkitive <sakshi.bhise@thinkitive.com>
2026-02-26 19:02:43 +0530

    variable in js

---
+++ var_js.js
@@ -0,0 +1,18 @@
+var name = "Sakshi";
+var age = 22;
+
+let city = "Kolhapur";
+city = "Pune";
+
+const country = "India";
+
+console.log(name);
+console.log(age);
+console.log(city);
+console.log(country);
+
+var x = 10;
+var x = 20;
+
+console.log(x);
+console.log(typeof(x));
